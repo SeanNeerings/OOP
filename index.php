a428ff8c19b80c3c9cb74e1d6f9e10471698d173
SeanNeerings <seanneerings1@gmail.com>
2022-06-01 14:09:42 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+class bottle {
+    public $name;
+    public $color;
+  
+    function __construct($name, $color) {
+      $this->name = $name;
+      $this->color = $color;
+    }
+    function get_name() {
+      return $this->name;
+    }
+    function get_color() {
+      return $this->color;
+    }
+  }
+
+  class metal_bottle extends bottle {
+    public $id;
+    public $use;
+  
+    function __construct($id, $use) {
+      $this->id = $id;
+      $this->use = $use;
+    }
+    function get_id() {
+      return $this->id;
+    }
+    function get_use() {
+      return $this->use;
+    }
+  }
+
+
+
+
+  
+  $bottle = new bottle("bottle", "red");
+  echo $bottle->get_color();
+  echo " ";
+  echo $bottle->get_name();
+    
+
+  
+  $metal_bottle = new metal_bottle("420", "koffie warm houden");
+  echo " ";
+  echo $metal_bottle->get_id();
+  echo " ";
+  echo $metal_bottle->get_use();
+
+
+?>
